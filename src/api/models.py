7d5f2b6ae12b1818f4a36f0a3011673d85eb4379
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-03-06 16:03:12 +0000

    cambio en el modelo1

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
